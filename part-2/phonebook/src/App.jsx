669588f4ec0b7b898f499c7d4da35c154313ee0f
aarsenovic <acawarrior@gmail.com>
2026-02-24 21:52:27 +0100

    part2 2.10

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState} from 'react'
+import Filter from '../components/Filter'
+import PersonForm from '../components/PersonForm'
+import Persons from '../components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -50,26 +53,15 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
 
-      <div>
-        filter shown with <input value={searchFilter} onChange={handleFilterChange}/>
-      </div>
+      <Filter searchFilter={searchFilter} handleFilterChange={handleFilterChange}/>
 
       <h2>Add new</h2>
-      <form onSubmit={addNewPhoneNumber}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit" >add</button>
-        </div>
-      </form>
+      <PersonForm onSubmit={addNewPhoneNumber} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
       ...
       {/* <div>debug {newName}</div> */}
-      {namesToShow.map(person=><div key={person.name}>{person.name} {person.number}</div>)}
+      {/* {namesToShow.map(person=><div key={person.name}>{person.name} {person.number}</div>)} */}
+      <Persons namesToShow={namesToShow}/>
     </div>
   )
 }
