021debded631db3ea669141cb300d3ea85bd38b7
aarsenovic <acawarrior@gmail.com>
2026-03-14 00:30:50 +0100

    part2 2.20

--- part-2/dataforcountries/src/App.jsx
+++ part-2/dataforcountries/src/App.jsx
@@ -5,7 +5,10 @@ import CountryView from "./components/CountryView"
 function App() {
   const [searchFilter, setSearchFilter] = useState('')
   const [countries, setCountries] = useState([])
+  const [weatherData, setWeatherData] = useState(null)
 
+
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY
 
   useEffect(() => {
     axios
@@ -15,12 +18,44 @@ function App() {
       })
   }, [])
 
+  useEffect(() => {
+ 
+
+
+    if (namesToShow !== null && namesToShow.length === 1) {
+      const capital = namesToShow[0].capital[0]
+      const cc = namesToShow[0].cca2.toLowerCase()
+
+      axios
+        .get(`http://api.openweathermap.org/data/2.5/weather?q=${capital},${cc}&APPID=${apiKey}`)
+        .then(response => {
+          setWeatherData(response.data)
+        })
+        .catch(error=>{
+          console.log("greska",error.response)
+        })
+    }
+
+
+
+    console.log("vreme",weatherData)
+
+ 
+  }, [searchFilter])
+
+
+
+
+
+
 
   const namesToShow = searchFilter ? countries.filter((country) => country.name.common.toLowerCase().includes(searchFilter.toLowerCase())) : null
 
 
   const handleFilterChange = (event) => {
     setSearchFilter(event.target.value);
+
+
   }
 
   const countriesToDisplay = () => {
@@ -33,14 +68,14 @@ function App() {
     if (namesToShow.length > 10) {
       return 'Too many matches, specify another filter'
     }
-    else if (namesToShow.length>1 &&  namesToShow.length <= 10) {
+    else if (namesToShow.length > 1 && namesToShow.length <= 10) {
       return (
         <ul>
           {namesToShow.map((country) => {
             return (
               <li key={country.ccn3}>
                 {/* {country.name.common} */}
-                <CountryView country={country}/>
+                <CountryView country={country} />
               </li>
             )
 
@@ -51,25 +86,34 @@ function App() {
 
     else if (namesToShow.length === 1) {
       return (
-      namesToShow.map(country=>{
-        return (
-          <div key={country.name.common}>
-            <h1>{country.name.common}</h1>
-            <div>{country.capital[0]}</div>
-            <div>Country: {country.area}</div>
-            <ul>
-              {Object.values(country.languages).map(language=>{
-                return(
-                  <li key={language}>{language}</li>
-                )
-              })}
-            </ul>
-            <div>
-              <img src={`${country.flags.png}`} alt={country.name.common} />{}
+        namesToShow.map(country => {
+          return (
+            <div key={country.name.common}>
+              <h1>{country.name.common}</h1>
+              <div>{country.capital[0]}</div>
+              <div>Country: {country.area}</div>
+              <ul>
+                {Object.values(country.languages).map(language => {
+                  return (
+                    <li key={language}>{language}</li>
+                  )
+                })}
+              </ul>
+              <div>
+                <img src={`${country.flags.png}`} alt={country.name.common} />{ }
+              </div>
+              <div>
+                Temperature - {weatherData ?weatherData.main.temp - 273.15 :"Loading..."}
+              </div>
+              <div>
+                Wind - {weatherData ?weatherData.wind.speed :"Loading..."}m/s
+              </div>
+              <div>
+                {weatherData ?<img src={`https://openweathermap.org/payload/api/media/file/${weatherData.weather[0].icon}.png`} alt="" /> :"loading"}
+              </div>
             </div>
-          </div>
-        )
-      })
+          )
+        })
       )
     }
   }
