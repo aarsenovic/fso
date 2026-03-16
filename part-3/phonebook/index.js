e1f525675ee5ae9c2cee4e24a8c48af7b97d4b96
aarsenovic <acawarrior@gmail.com>
2026-03-16 11:40:01 +0100

    part3 3.3

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -5,22 +5,22 @@ const app = express()
 
 let persons = [
     {
-        id: 1,
+        id: "1",
         name: "Arto Hellas",
         number: "040-123456"
     },
     {
-        id: 2,
+        id: "2",
         name: "Ada Lovelace",
         number: "39-44-5323523"
     },
     {
-        id: 3,
+        id: "3",
         name: "Dan Abramov",
         number: "12-43-234345"
     },
     {
-        id: 4,
+        id: "4",
         name: "Mary Poppendieck",
         number: "39-23-6423122"
     }
@@ -37,6 +37,19 @@ app.get('/info', (request, response)=> {
         `)
 
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+
+    const person = persons.find(person => person.id === id)
+
+    if(person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 
 
 const PORT = 3001
