b3a809db1897e58fb07bc31023d3cc41f3b4b986
aarsenovic <acawarrior@gmail.com>
2026-03-21 01:39:34 +0100

    part3 3.12

---
+++ part-3/phonebook/mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const password = encodeURIComponent(process.argv[2])
+
+const url = `mongodb+srv://acawarrior_db_user:${password}@cluster0.fryo5ti.mongodb.net/?appName=Cluster0`
+
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, {family: 4})
+
+
+const personSchema = new mongoose.Schema ({
+    name: String,
+    number: String,
+})
+
+
+const Person = mongoose.model('Person', personSchema)
+
+
+if(process.argv.length === 3) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(
+                `${person.name} ${person.number}`
+            )
+        })
+        mongoose.connection.close()
+    })
+}
+
+
+
+if (process.argv.length === 5) {
+    const person = new Person ({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(result => {
+        console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+
+
+
+
+
