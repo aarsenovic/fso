9a6f12a801ba3ecc5bfd1963b7799234878406f7
aarsenovic <acawarrior@gmail.com>
2026-03-23 17:33:51 +0100

    part3 3.16

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -68,12 +68,12 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
         response.status(204).end()
     })
-    .catch(error => console.log(error))
+    .catch(error => next(error))
 })
 
 
@@ -112,6 +112,20 @@ app.post('/api/persons', (request, response) => {
 
 
 })
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+
+app.use(errorHandler)
 
 
 
