a279fb544f661271516099fc1838b63ed2c9cb08
aarsenovic <acawarrior@gmail.com>
2026-03-23 18:08:54 +0100

    part3 3.17

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -114,6 +114,26 @@ app.post('/api/persons', (request, response) => {
 })
 
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const {name, number} = request.body
+
+    Person.findById(request.params.id)
+    .then(person => {
+        if (!person) {
+            return response.status(404).end()
+        }
+
+        person.name = name
+        person.number = number
+
+        return person.save().then((updatedPerson) => {
+            response.json(updatedPerson)
+        })
+    })
+    .catch(error => next(error))
+})
+
+
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
