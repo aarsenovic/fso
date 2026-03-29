5bfe280d9127e87adb931b6f82309d4231a49dbf
aarsenovic <acawarrior@gmail.com>
2026-03-29 20:37:52 +0200

    part4 4.2

---
+++ part-4/bloglist/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+
+const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+
+  app.use(express.json())
+  app.use(middleware.requestLogger)
+
+
+app.use('/api/blogs', blogRouter)
+
+
+module.exports = app
