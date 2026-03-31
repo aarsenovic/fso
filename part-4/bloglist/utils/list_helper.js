35dd82fff590e6fd11d30cf61cf60d63c92b2c27
aarsenovic <acawarrior@gmail.com>
2026-03-31 09:53:41 +0200

    part4 4.4

--- part-4/bloglist/utils/list_helper.js
+++ part-4/bloglist/utils/list_helper.js
@@ -3,6 +3,18 @@ const dummy = (blogs) => {
 }
 
 
+const totalLikes = (blogList) => {
+    const reducer = ( sum, item ) => {
+        return sum  + item.likes
+    }
+
+    return blogList.length === 0
+    ? 0
+    : blogList.reduce(reducer,  0)
+}
+
+
 module.exports = {
-    dummy 
+    dummy,
+    totalLikes 
 }
