ff2e567d83eed4e70002c4672d7fdc004affdd09
aarsenovic <acawarrior@gmail.com>
2026-04-14 21:23:09 +0200

    part4 4.10

--- part-4/bloglist/tests/blog_api.test.js
+++ part-4/bloglist/tests/blog_api.test.js
@@ -19,30 +19,53 @@ beforeEach(async () => {
 })
 
 
-test('blogs are returned as json', async() => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-} )
+})
 
-test('all blogs are returned', async() => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
 
-test("blog post's unique identifier is called id", async() => {
+test("blog post's unique identifier is called id", async () => {
   const response = await api.get('/api/blogs')
   const allHaveId = response.body.every(object => Object.hasOwn(object, 'id') && !Object.hasOwn(object, '_id'))
 
   assert.strictEqual(allHaveId, true)
 })
 
+test('a valid blog post can be added', async () => {
+  const newBlog = {
+    title: 'Archlinux is the best',
+    author: 'Linus Torvalds',
+    url: 'https://archlinux.org/linux-is-cool',
+    likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const contents = blogsAtEnd.map(n => n.title)
+
+  assert.strictEqual(contents.includes('Archlinux is the best'), true)
+
+})
 
 
 
 
-after(async() => {
+after(async () => {
   await mongoose.connection.close()
 })
