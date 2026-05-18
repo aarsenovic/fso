1902373e0863d033e6d30aeae474bb3a2762e894
aarsenovic <acawarrior@gmail.com>
2026-05-18 19:18:39 +0200

    part5 5.6

--- part-5/bloglist-frontend/src/components/NewBlogForm.jsx
+++ part-5/bloglist-frontend/src/components/NewBlogForm.jsx
@@ -7,9 +7,7 @@ const NewBlogForm = ({ blogs, setBlogs, setMessage, toggleVisibility }) => {
     const [author, setAuthor] = useState('')
     const [url, setUrl] = useState('')
 
-
-
-
+    
     const handleBlogCreation = async event => {
         event.preventDefault()
         
@@ -44,8 +42,6 @@ const NewBlogForm = ({ blogs, setBlogs, setMessage, toggleVisibility }) => {
 
 
     }
-
-
 
     return (
         <>
