0e43991b19657982523cd60018bf5b3aae0cc0fb
aarsenovic <acawarrior@gmail.com>
2026-05-19 21:20:17 +0200

    part5 5.7

--- part-5/bloglist-frontend/src/components/Blog.jsx
+++ part-5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+
+  const [shown, setShown] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+const toggleVisibility = () => {
+  setShown(prev => !prev)
+}
+
+  return (
+
+    <div style={blogStyle}>
+      <div>
+        {blog.title}<button onClick={toggleVisibility}>{shown ?'hide' :'show'}</button>
+      </div>
+      {shown&&
+      <div>
+        <div>{blog.url}</div>
+        <div>{blog.likes}<button>like</button></div>
+        <div>{blog.author}</div>  
+      </div>}
+  </div>
+)}
+
 
 export default Blog
