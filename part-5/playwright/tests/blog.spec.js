18202c863c46bb1dde96abab42e6e72ad924812a
aarsenovic <acawarrior@gmail.com>
2026-07-01 19:30:27 +0200

    part5 5.18

--- part-5/playwright/tests/blog.spec.js
+++ part-5/playwright/tests/blog.spec.js
@@ -1,7 +1,15 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'tester',
+        username: 'testuser',
+        password: 'tester123'
+      }
+    })
     await page.goto('http://localhost:5173')
   })
 
@@ -13,4 +21,29 @@ describe('Blog app', () => {
     await expect(page.getByLabel('password')).toBeVisible()
 
   })
+
+  describe('Login', () => {
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('mluukai')
+      await page.getByLabel('password').fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('wrong credentials')).toBeVisible()
+    })
+
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('testuser')
+      await page.getByLabel('password').fill('tester123')
+
+      await page.getByRole('button', { name: 'login' }).click()
+      await expect(page.getByText('login successful')).toBeVisible()
+    })
+  })
+
+
+
 })
+
+
+
+
