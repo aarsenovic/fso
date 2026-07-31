eb3d7ff411a16f6a09c0f3f27cbb24f3d9afcd4a
aarsenovic <acawarrior@gmail.com>
2026-07-31 22:30:27 +0200

    part5 5.31

--- part-5/bloglist-frontend/src/components/Blog.jsx
+++ part-5/bloglist-frontend/src/components/Blog.jsx
@@ -1,12 +1,42 @@
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
+
+const StyledHeader = styled.div`
+font-size: 25px;
+margin-top: 15px;
+margin-bottom: 15px;
+color: black;
+`
+const StyledParagraph = styled.div`
+font-size: 15px;
+color: gray;
+margin-top: 15px;
+margin-bottom: 15px;
+`
+
+const StyledLike = styled.span`
+font-size: 15px; 
+color: darkblue;
+margin-right: 15px;
+`
+
+const StyledLikeButton = styled.button`
+padding: 8px;
+color: darkblue;
+border: 2px solid light blue;
+font-size: 15px
+`
+const StyledDeleteButton = styled.button`
+padding: 8px;
+color: red;
+border: 2px solid red;
+font-size: 15px;
+margin-left: 20px;
+`
 
 const Blog = ({ blog, user, handleLike, handleDelete }) => {
 
-  const [shown, setShown] = useState(false)
-  const id = useParams().id
-  // const blog = blogs.find(b => b.id === id)
+
 
   const blogStyle = {
     paddingTop: 10,
@@ -16,9 +46,6 @@ const Blog = ({ blog, user, handleLike, handleDelete }) => {
     marginBottom: 5
   }
 
-  // const toggleVisibility = () => {
-  //   setShown(prev => !prev)
-  // }
 
   if (!blog) {
     return null
@@ -30,14 +57,14 @@ const Blog = ({ blog, user, handleLike, handleDelete }) => {
     <div style={blogStyle} className='blog'>
       <div>
         {/* <button onClick={toggleVisibility}>{shown ? 'hide' : 'show'}</button> */}
-        <div>{blog.title}</div>
-        <div>{blog.author}</div>
+        <StyledHeader>{blog.title}</StyledHeader>
+        <StyledParagraph>{blog.author}</StyledParagraph>
       </div>
       {/* {shown && */}
       <div>
-        <div>{blog.url}</div>
-        {user ?<div><span data-testid="likes-count">{blog.likes}</span><button onClick={() => handleLike(blog)}>like</button></div>  :null}
-        {user?.username === blog.user.username ?<button onClick={() => handleDelete(blog)}>Remove</button> :null}
+        <StyledParagraph>{blog.url}</StyledParagraph>
+        {user ?<div><StyledLike data-testid="likes-count">{blog.likes}</StyledLike><StyledLikeButton onClick={() => handleLike(blog)}>like</StyledLikeButton></div>  :null}
+        {user?.username === blog.user.username ?<StyledDeleteButton onClick={() => handleDelete(blog)}>Remove</StyledDeleteButton> :null}
       </div>
     </div>
   )
